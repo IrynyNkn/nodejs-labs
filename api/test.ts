35273dc27bc03d056c8743e4c989cf2df8b29bee
Iryna Nakonechna <iryninak03@gmail.com>
2022-10-16 11:33:53 +0300

    Checking husky

--- api/test.ts
+++ api/test.ts
@@ -1,4 +1,6 @@
 const msg = 'Hello world ;)';
+const unusedVal = 'nobody uses me:(';
+console.log('Message: ', msg);
 
 export default function handler(req: any, res: any) {
   res.status(200).send({ msg });
