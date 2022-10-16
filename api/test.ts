27d5d2435e1ee424823d99d37e542146c67f0a9c
Iryna Nakonechna <iryninak03@gmail.com>
2022-10-16 16:17:15 +0300

    added types for handler

--- api/test.ts
+++ api/test.ts
@@ -1,5 +1,7 @@
+import type { VercelRequest, VercelResponse } from '@vercel/node'
+
 const msg = 'Hello world ;)'
 
-export default function handler(req: any, res: any) {
+export default function handler(req: VercelRequest, res: VercelResponse) {
   res.status(200).send({ msg })
 }
