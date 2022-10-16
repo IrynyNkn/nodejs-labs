a339ed23ec170b8777621901ee20dac5b28293f8
Iryna Nakonechna <iryninak03@gmail.com>
2022-10-16 12:46:51 +0300

    test vercel deploy

--- api/test.ts
+++ api/test.ts
@@ -1,6 +1,4 @@
 const msg = 'Hello world ;)';
-const unusedVal = 'nobody uses me:(';
-console.log('Message: ', msg);
 
 export default function handler(req: any, res: any) {
   res.status(200).send({ msg });
