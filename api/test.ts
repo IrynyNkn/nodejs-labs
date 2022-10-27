9fa35237713ebeb481b9ef96db7c36ef0c677cad
Iryna Nakonechna <iryninak03@gmail.com>
2022-10-27 10:29:35 +0300

    changed env option to node and improved gitignore

--- api/test.ts
+++ api/test.ts
@@ -3,5 +3,5 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 const msg = 'Hello world ;)'
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).send({ msg })
+  res.status(200).send(msg)
 }
