3c953ee5cff28f891fb932a3c4e141345c76ea1c
Iryna Nakonechna <iryninak03@gmail.com>
2022-11-20 19:25:00 +0200

    added nested route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,16 +2,16 @@ import Router from '../Router'
 
 const router = new Router('/api')
 
-router.get('/', (req, res) => {
-  res.send({ message: 'i am a root route :)' })
-})
-
 router.get('/test', (req, res) => {
   res.send({ message: 'i am a test route :)' })
 })
 
 router.post('/test', (req, res, payload) => {
   res.send(payload)
+})
+
+router.get('/test/me', (req, res) => {
+  res.send({ message: 'i am a nested test route, thank you for testing me' })
 })
 
 export default router
